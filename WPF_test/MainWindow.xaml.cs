f47abe7635ef494d2da666aa52d0a50b525646aa
KarasuZero <60535595+KarasuZero@users.noreply.github.com>
2022-10-30 04:26:00 -0600

    ver 0.3  basic wpf application that allows user to move the image around the screen with bt interactions
     *todos added
     *need to implement command pattern

--- WPF_test/MainWindow.xaml.cs
+++ WPF_test/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
-
 namespace WPF_test
 {
     /// <summary>
@@ -30,8 +29,10 @@ namespace WPF_test
             }
             return output;
         }
+        
         private void BtUp_OnClick(object sender, RoutedEventArgs e) //row operation
         {
+            //TODO handle edge cases
             //move image 1 to the top
             Grid.SetRow(TheImage, Grid.GetRow(TheImage) - 1);
             _commandStack.Push("Up");
@@ -41,6 +42,8 @@ namespace WPF_test
 
         private void BtDown_OnClick(object sender, RoutedEventArgs e) //row operation
         {
+            //TODO handle edge cases
+            
             Grid.SetRow(TheImage, Grid.GetRow(TheImage) + 1);
             _commandStack.Push("Down");
             
@@ -49,6 +52,8 @@ namespace WPF_test
         
         private void BtLeft_OnClick(object sender, RoutedEventArgs e)
         {
+            //TODO handle edge cases
+            
             Grid.SetColumn(TheImage, Grid.GetColumn(TheImage) - 1);
             _commandStack.Push("Left");
             
@@ -57,6 +62,8 @@ namespace WPF_test
         
         private void BtRight_OnClick(object sender, RoutedEventArgs e)
         {
+            //TODO handle edge cases
+            
             Grid.SetColumn(TheImage, Grid.GetColumn(TheImage) + 1);
             _commandStack.Push("Right");
             
@@ -65,11 +72,14 @@ namespace WPF_test
 
         private void BtRe_OnClick(object sender, RoutedEventArgs e)
         {
+            //TODO implement undo
             throw new NotImplementedException();
         }
         
         private void BtBG_OnClick(object sender, RoutedEventArgs e)
         {
+            //TODO handle edge cases
+            
             TheImage.Source = new BitmapImage(new Uri("C:\\Users\\kento\\RiderProjects\\CSC360\\katanaZero.jpg"));
             _commandStack.Push("BGChange");
             
